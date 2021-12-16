83786fda4d3d8294870d060581775c7b428f2101
tomasvicente20002 <56982030+tomasvicente20002@users.noreply.github.com>
2021-12-16 11:29:02 +0000

    correcao na incializacao do estrtura

--- BP/main.c
+++ BP/main.c
@@ -6,19 +6,22 @@
 laureate_t* getNewLiveLaureate()
 {
 	laureate_t* ptr = malloc(sizeof(laureate_t));
-	ptr->id = 0;
-	ptr->firstname[0] = '\n';
-	ptr->surname[0] = '\n';
-	ptr->birthdate[0] = '\n';
-	ptr->Country[0] = '\n';
-	ptr->CountryCode[0] = '\n';
-	ptr->City[0] = '\n';
-	ptr->died[0] = '\n';
-	ptr->gender[0] = '\n';
-	ptr->year = 0;
-	ptr->category[0] = '\n';
-	ptr->share = 0;
-	ptr->motivation[0] = '\n';
+	if (ptr != NULL)
+	{
+		ptr->id = 0;
+		ptr->firstname[0] = '\n';
+		ptr->surname[0] = '\n';
+		ptr->birthdate[0] = '\n';
+		ptr->Country[0] = '\n';
+		ptr->CountryCode[0] = '\n';
+		ptr->City[0] = '\n';
+		ptr->died[0] = '\n';
+		ptr->gender[0] = '\n';
+		ptr->year = 0;
+		ptr->category[0] = '\n';
+		ptr->share = 0;
+		ptr->motivation[0] = '\n';
+	}
 	return ptr;
 }
 
