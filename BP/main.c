f5a8d3d6098e2ded298dfd8e2b576c7dab68d31c
tomasvicente20002 <56982030+tomasvicente20002@users.noreply.github.com>
2021-12-16 10:30:53 +0000

    First commit

---
+++ BP/main.c
@@ -0,0 +1,12 @@
+﻿// main.c : Defines the entry point for the application.
+//
+
+#include "main.h"
+
+
+
+int main()
+{
+;
+	return 0;
+}
---
+++ BP/main.h
@@ -0,0 +1,24 @@
+﻿// main.h : Include file for standard system include files,
+// or project specific include files.
+#pragma once
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef struct laureate_st {
+	uint32_t id;
+	char firstname[100];
+	char surname[100];
+	char birthdate[11];
+	char Country[50];
+	char CountryCode[3];
+	char City[50];
+	char died[11];
+	char gender[7];
+	uint32_t year;
+	char category[12];
+	uint32_t share;
+	char motivation[350];
+} laureate_t;
+
+// TODO: Reference additional headers your program requires here.
